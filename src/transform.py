ed98f8a93d2cb8af4cb39db898f26b55ea5a71b2
mike0sv <mike0sv@gmail.com>
2020-09-07 03:11:30 +0300

    fix shapes

--- src/transform.py
+++ src/transform.py
@@ -2,6 +2,7 @@ import tensorflow as tf, pdb
 
 WEIGHTS_INIT_STDEV = .1
 
+
 def net(image):
     conv1 = _conv_layer(image, 32, 9, 1)
     conv2 = _conv_layer(conv1, 64, 3, 2)
@@ -14,8 +15,9 @@ def net(image):
     conv_t1 = _conv_tranpose_layer(resid5, 64, 3, 2)
     conv_t2 = _conv_tranpose_layer(conv_t1, 32, 3, 2)
     conv_t3 = _conv_layer(conv_t2, 3, 9, 1, relu=False)
-    preds = tf.nn.tanh(conv_t3) * 150 + 255./2
+    preds = tf.nn.tanh(conv_t3) * 150 + 255. / 2
     return preds
+
 
 def _conv_layer(net, num_filters, filter_size, strides, relu=True):
     weights_init = _conv_init_vars(net, num_filters, filter_size)
@@ -27,37 +29,47 @@ def _conv_layer(net, num_filters, filter_size, strides, relu=True):
 
     return net
 
+
 def _conv_tranpose_layer(net, num_filters, filter_size, strides):
-    weights_init = _conv_init_vars(net, num_filters, filter_size, transpose=True)
+    weights_init = _conv_init_vars(net, num_filters, filter_size)
+    weights_init2 = _conv_init_vars(net, num_filters, filter_size)
 
     batch_size, rows, cols, in_channels = [i for i in net.get_shape()]
     new_rows, new_cols = int(rows * strides), int(cols * strides)
     # new_shape = #tf.pack([tf.shape(net)[0], new_rows, new_cols, num_filters])
 
     new_shape = [batch_size, new_rows, new_cols, num_filters]
-    # tf_shape = tf.stack(new_shape)
-    strides_shape = [1,strides,strides,1]
+    tf_shape = tf.stack(new_shape)
+    strides_shape = [1, strides, strides, 1]
 
-    # net = tf.nn.conv2d_transpose(net, weights_init, tf_shape, strides_shape, padding='SAME')
+    # kek_shape = net.get_shape()
+    #
+    net2 = tf.nn.conv2d_transpose(net, weights_init, tf_shape, strides_shape, padding='SAME')
+
     net = tf.image.resize(net, (new_rows, new_cols))
-    net = tf.nn.conv2d(net, weights_init, strides_shape, padding='SAME')
-    # net = tf.nn.conv2d()
+    net = tf.nn.conv2d(net, weights_init, [1, 1, 1, 1], padding='SAME')
+    # net = tf.nn.relu(net)
+    # net = tf.nn.conv2d(net, weights_init2, [1, 1, 1, 1], padding='SAME')
+    assert net2.get_shape() == net.get_shape(), f'{net2.get_shape()} != {net.get_shape()}, {new_shape}'
     net = _instance_norm(net)
     return tf.nn.relu(net)
+
 
 def _residual_block(net, filter_size=3):
     tmp = _conv_layer(net, 128, filter_size, 1)
     return net + _conv_layer(tmp, 128, filter_size, 1, relu=False)
 
+
 def _instance_norm(net, train=True):
     batch, rows, cols, channels = [i for i in net.get_shape()]
     var_shape = [channels]
-    mu, sigma_sq = tf.nn.moments(x=net, axes=[1,2], keepdims=True)
+    mu, sigma_sq = tf.nn.moments(x=net, axes=[1, 2], keepdims=True)
     shift = tf.Variable(tf.zeros(var_shape))
     scale = tf.Variable(tf.ones(var_shape))
     epsilon = 1e-3
-    normalized = (net-mu)/(sigma_sq + epsilon)**(.5)
+    normalized = (net - mu) / (sigma_sq + epsilon) ** (.5)
     return scale * normalized + shift
+
 
 def _conv_init_vars(net, out_channels, filter_size, transpose=False):
     _, rows, cols, in_channels = [i for i in net.get_shape()]
@@ -66,5 +78,6 @@ def _conv_init_vars(net, out_channels, filter_size, transpose=False):
     else:
         weights_shape = [filter_size, filter_size, out_channels, in_channels]
 
-    weights_init = tf.Variable(tf.random.truncated_normal(weights_shape, stddev=WEIGHTS_INIT_STDEV, seed=1), dtype=tf.float32)
+    weights_init = tf.Variable(tf.random.truncated_normal(weights_shape, stddev=WEIGHTS_INIT_STDEV, seed=1),
+                               dtype=tf.float32)
     return weights_init
